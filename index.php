021819fbd82448b0561e7e9f2b7809cef3344ef4
NicolaiHjacobsen <43333217+NicolaiHjacobsen@users.noreply.github.com>
2019-03-18 14:36:53 +0100

    first upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>LoginSystem</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    
+</head>
+<body>
+
+    <div class="navbar">
+        <nav>
+            <ul>
+                <li><a href="">lol</a></li>
+            </ul>
+        </nav>
+    </div>
+    
+</body>
+</html>
